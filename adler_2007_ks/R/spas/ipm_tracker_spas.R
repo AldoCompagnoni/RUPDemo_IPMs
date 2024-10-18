1ea9f147505441c290bc7b269a51c2f8c46460a8
NeisseN <neisse.n@protonmail.com>
2024-10-18 15:30:04 +0200

    get adler 2007 kansas Sporobolus asper !mean! ready

--- adler_2007_ks/R/spas/ipm_tracker_spas.R
+++ adler_2007_ks/R/spas/ipm_tracker_spas.R
@@ -1,11 +1,11 @@
-# IPM for alder 2007; kansas; Sporobolus cryptandrus
+# IPM for alder 2007; kansas; Sporobolus asper
 
 # Author: Niklas Neisse
 # Email: neisse.n@protonmail.com
 # Date: 2024.10.17
 
 # Process plant growth data to explore vital rates for 
- # the species Sporobolus cryptandrus and 
+ # the species Sporobolus asper and 
  # generates visualizations and model fits for IPM analysis.
 
 # Setting the stage ------------------------------------------------------------
@@ -18,7 +18,7 @@ options(stringsAsFactors = F)
 
 # Packages ---------------------------------------------------------------------
 # List of required CRAN packages
-.cran_packages <- c('tidyverse','patchwork','skimr') 
+.cran_packages <- c('tidyverse','patchwork','skimr', 'ipmr') 
 # Check if all required packages are installed
 .inst <- .cran_packages %in% installed.packages() 
 if(any(!.inst)) {
@@ -30,7 +30,7 @@ sapply(.cran_packages, require, character.only = TRUE)
 
 # Data -------------------------------------------------------------------------
 # Specify the species being analyzed
-species <- 'Sporobolus cryptandrus'
+species <- 'Sporobolus asper'
 # Create a unique species abbreviation for file naming
 sp_abb  <- tolower(
   gsub(" ", "", paste(substr(unlist(strsplit(species, " ")), 1, 2), 
@@ -198,7 +198,7 @@ ggsave(paste0("adler_2007_ks/results/", sp_abb, "/overall_gr.png"),
 
 # Recruitment analysis
 # Create a scatter plot showing the relationship between 
-# total parent plant area and number of recruits
+ # total parent plant area and number of recruits
 rec_overall <- 
   ggplot(recr_df, aes(x = tot_p_area, y = nr_quad)) + 
   geom_point(alpha = 0.5, pch = 16, size = 1, color = 'red') +  
@@ -212,24 +212,35 @@ ggsave(paste0("adler_2007_ks/results/", sp_abb, "/overall_rec.png"),
        width = 4, height = 3, units = "in", dpi = 150)
 
 
-# Fitting vital rate models for the mean IPM -----------------------------------
-# Fitting growth models to predict size at time t1 based on size at time t0
+# Fit vital rate models for the mean IPM -----------------------------------
+ # Fit growth models to predict size at time t1 based on size at time t0
 # Linear model
-gr_mod_mean    <- 
+gr_mod_mean   <- 
   lm(logsize_t1 ~ logsize_t0, data = grow_df)
 # Quadratic model
-gr_mod_mean_2  <- 
+gr_mod_mean_2 <- 
   lm(logsize_t1 ~ logsize_t0 + logsize_t0_2, data = grow_df)  
 # Cubic model
-gr_mod_mean_3  <- 
+gr_mod_mean_3 <- 
   lm(logsize_t1 ~ logsize_t0 + logsize_t0_2 + logsize_t0_3, data = grow_df)  
-# Compare models using ANOVA
-anova(gr_mod_mean, gr_mod_mean_2, gr_mod_mean_3)
+# Compare models using AIC
+gr_mods       <- list(gr_mod_mean, gr_mod_mean_2, gr_mod_mean_3)
+gr_aic        <- sapply(gr_mods, AIC)
+# Assign the best model
+gr_mod_bestfit_index <- which.min(gr_aic)
+gr_mod_bestfit       <- gr_mods[[gr_mod_bestfit_index]]
+gr_ranef             <- coef(gr_mod_bestfit)
 
 # Predict size at time t1 using the mean growth model
-grow_df$pred <- predict(gr_mod_mean, type = "response")
+grow_df$pred <- predict(gr_mod_bestfit, type = "response")
 
 # Plot observed size at time t1 against size at time t0 with the fitted line
+if (       length(gr_ranef) == 2) {line_color <- 'red'
+} else if (length(gr_ranef) == 3) {line_color <- 'green'
+} else if (length(gr_ranef) == 4) {line_color <- 'blue'
+} else {                           line_color <- 'black'
+}
+
 grow_line <- 
   ggplot(grow_df, aes(x = logsize_t0, y = logsize_t1)) +
   # Plot observed data
@@ -237,7 +248,7 @@ grow_line <-
   # Add fitted regression line
   geom_abline(aes(intercept = coef(gr_mod_mean)[1],  
                   slope     = coef(gr_mod_mean)[2]),
-              color= 'red', lwd = 2) +
+              color= line_color, lwd = 2) +
   theme_bw()
 
 # Plot predicted versus observed size at time t1
@@ -252,7 +263,8 @@ grow_pred <-
 grow_overall_pred <- grow_line + grow_pred + plot_layout() 
 
 # Save the growth prediction plot
-ggsave(paste0("adler_2007_ks/results/", sp_abb, "/overall_grow_pred.png"), 
+ggsave(paste0("adler_2007_ks/results/", sp_abb, 
+              "/overall_grow_pred_logs", gr_mod_bestfit_index, ".png"), 
        plot = grow_overall_pred, 
        width = 8, height = 4, units = "in", dpi = 150)
 
@@ -265,30 +277,47 @@ y         <- resid(gr_mod_mean)^2
 gr_var_m  <- nls(y ~ a * exp(b * x), start = list(a = 1, b = 0))  
 
 # Survival
-# Fit models to predict survival based on size at time t0
+ # Fit models to predict survival based on size at time t0
 # Logistic regression
 su_mod_mean   <- glm(survives ~ logsize_t0, 
-                     data = surv_df, family = "binomial" ) 
+                     data = surv_df, family = "binomial") 
 # Quadratic logistic model
 su_mod_mean_2 <- glm(survives ~ logsize_t0 + logsize_t0_2, 
-                     data = surv_df, family = "binomial" )  
+                     data = surv_df, family = "binomial")  
 # Cubic logistic model
 su_mod_mean_3 <- glm(survives ~ logsize_t0 + logsize_t0_2 + logsize_t0_3, 
-                     data = surv_df, family = "binomial" )  
-# Compare models using ANOVA
-anova(su_mod_mean, su_mod_mean_2, su_mod_mean_3)
+                     data = surv_df, family = "binomial")  
+# Compare models using AIC
+su_mods       <- list(su_mod_mean, su_mod_mean_2, su_mod_mean_3)
+su_aic        <- sapply(su_mods, AIC)
+# Assign the best model
+su_mod_bestfit_index <- which.min(su_aic)
+su_mod_bestfit       <- su_mods[[su_mod_bestfit_index]]
+su_ranef             <- coef(su_mod_bestfit)
 
 # Generate predictions for survival across a range of sizes
 surv_x <- seq(min(surv_df$logsize_t0, na.rm = T), 
               max(surv_df$logsize_t0, na.rm = T), length.out = 100)
 # Inverse logit for predictions
-surv_pred <- boot::inv.logit(coef(su_mod_mean)[1] + 
-                               coef(su_mod_mean)[2] * surv_x)  
+su_linear_predictor <- su_ranef[1] 
+if (length(su_ranef) >= 2) {
+  su_linear_predictor <- su_linear_predictor + su_ranef[2] * surv_x}
+if (length(su_ranef) >= 3) {
+  su_linear_predictor <- su_linear_predictor + su_ranef[3] * surv_x^2}
+if (length(su_ranef) >= 4) {
+  su_linear_predictor <- su_linear_predictor + su_ranef[4] * surv_x^3}
+surv_pred <- boot::inv.logit(su_linear_predictor)
 
 # Prepare data for survival plot
 surv_pred_df <- data.frame(logsize_t0 = surv_x, survives = surv_pred)
 
 # Plot observed survival with fitted line
+if (       length(su_ranef) == 2) {line_color <- 'red'
+} else if (length(su_ranef) == 3) {line_color <- 'green'
+} else if (length(su_ranef) == 4) {line_color <- 'blue'
+} else {                           line_color <- 'black'
+}
+
 surv_line <- 
   ggplot() +
   geom_jitter(data = surv_df, aes(x = logsize_t0, 
@@ -296,7 +325,7 @@ surv_line <-
               alpha = 0.25, width = 0, height = 0.25) + 
   geom_line(data = surv_pred_df, aes(x = logsize_t0, 
                                      y = survives),
-            color = 'red', lwd   = 2) +  
+            color = line_color, lwd   = 2) +  
   theme_bw()
 
 # Plot binned survival proportions with error bars
@@ -320,7 +349,8 @@ surv_bin <-
 surv_overall_pred <- surv_line + surv_bin + plot_layout()
 
 # Save the survival prediction plot
-ggsave(paste0("adler_2007_ks/results/", sp_abb, "/overall_surv_pred.png"), 
+ggsave(paste0("adler_2007_ks/results/", sp_abb, 
+              "/overall_surv_pred_logs", su_mod_bestfit_index, ".png"), 
        plot = surv_overall_pred, 
        width = 8, height = 3, units = "in", dpi = 150) 
 
@@ -354,3 +384,280 @@ repr_pc_m <- indiv_m %>%
 
 # Output the reproduction per capita summary
 repr_pc_m
+
+# Exporting parameter estimates ------------------------------------------------
+# Growth
+grow_fe <- data.frame(coefficient = names(coef(gr_mod_bestfit)),
+                      value       = coef(gr_mod_bestfit))
+var_m   <- data.frame(coefficient = names(coef(gr_var_m)),
+                      value       = coef(gr_var_m))
+
+grow_out <- Reduce(function(...) rbind(...), list(grow_fe, var_m)) %>%
+  mutate(coefficient = as.character(coefficient)) %>%
+  mutate(coefficient = replace(coefficient, 
+                                 grepl("Intercept", coefficient), "b0"))
+
+write.csv(grow_out, 
+          paste0("adler_2007_ks/data/", sp_abb, "/grow_pars_mean.csv"), 
+          row.names = F)
+
+# Survival
+surv_fe <- data.frame(coefficient = names(coef(su_mod_bestfit)),
+                      value       = coef(su_mod_bestfit))
+
+surv_out<- Reduce(function(...) rbind(...), list(surv_fe)) %>%
+  mutate(coefficient = as.character(coefficient)) %>%
+  mutate(coefficient = replace(coefficient, 
+                                 grepl("Intercept", coefficient), "b0"))
+write.csv(surv_out, 
+          paste0("adler_2007_ks/data/", sp_abb, "/surv_pars_mean.csv"), 
+          row.names = F)
+
+# Recruitment 
+rec_size <- df %>% subset(recruit == 1)
+
+others   <- data.frame(coefficient = c("rec_siz", "rec_sd", 
+                                       "max_siz", "min_siz",
+                                       "fecu_b0"),
+                       value       = c(mean(log(rec_size$size_t0)), 
+                                       sd(  log(rec_size$size_t0)),
+                                       grow_df$logsize_t0 %>% max, 
+                                       grow_df$logsize_t0 %>% min,
+                                       repr_pc_m$repr_pc_mean))
+
+write.csv(others, 
+          paste0("adler_2007_ks/data/", sp_abb, "/other_pars_mean.csv"), 
+          row.names = F)
+
+
+# Building the IPM from scratch ------------------------------------------------
+extr_value <- function(x, field){subset(x, coefficient == field)$value}
+
+pars  <- list(surv_b0 = extr_value(surv_out, "b0"),
+                   surv_b1 = extr_value(surv_out, "logsize_t0"),
+                   grow_b0 = extr_value(grow_out, "b0"),
+                   grow_b1 = extr_value(grow_out, "logsize_t0"),
+                   a       = extr_value(grow_out, "a"),
+                   b       = extr_value(grow_out, "b"),
+                   fecu_b0 = extr_value(others, "fecu_b0"),
+                   recr_sz = extr_value(others, "rec_siz"),
+                   recr_sd = extr_value(others, "rec_sd"),
+                   L       = extr_value(others, "min_siz"),
+                   U       = extr_value(others, "max_siz"),
+                   mat_siz = 200)
+
+# Dynamically add surv_b2, surv_b3, grow_b2, grow_b3 if they exist
+for (coef_suffix in c("b2", "b3")) {
+  surv_coef_suffix <- paste0("surv_", coef_suffix)
+  grow_coef_suffix <- paste0("grow_", coef_suffix)
+  
+  surv_value <- extr_value(surv_out, surv_coef_suffix)
+  if (!is.null(surv_value) && length(surv_value) > 0) {
+    pars[[surv_coef_suffix]] <- surv_value
+  }
+  
+  grow_value <- extr_value(grow_out, grow_coef_suffix)
+  if (!is.null(grow_value) && length(grow_value) > 0) {
+    pars[[grow_coef_suffix]] <- grow_value
+  }
+}
+
+write.csv(pars, 
+          paste0("adler_2007_ks/data/", sp_abb, "/pars.csv"), 
+          row.names = F)
+
+
+# Function describing standard deviation of growth model
+grow_sd <- function(x, pars) {
+  pars$a * (exp(pars$b* x)) %>% sqrt 
+}
+
+# !!! Check !!! Function describing growth from size x to size y ####
+gxy <- function(x, y, pars) {
+  return(dnorm(y, mean = pars$grow_b0 + pars$grow_b1*x,
+               sd = grow_sd(x, pars)))
+}
+
+# Function describing the invert logit
+inv_logit <- function(x) {exp(x) / (1 + exp(x))}
+
+# !!! Check !!! Function describing survival from size t0 to t1
+sx <- function(x, pars) {
+  return(inv_logit(pars$surv_b0 + pars$surv_b1 * x))
+}
+
+# Function describing the transition kernel
+pxy <- function(x, y, pars) {
+  return(sx(x, pars) * gxy(x, y, pars))
+}
+
+# Function describing the recruitment 
+fy <- function(y, pars, h){
+  n_recr  <- pars$fecu_b0
+  recr_y  <- dnorm(y, pars$recr_sz, pars$recr_sd) * h
+  recr_y  <- recr_y / sum(recr_y)
+  f       <- n_recr * recr_y
+  return(f)
+}
+
+# Kernel
+kernel <- function(pars) {
+  # number of bins over which to integrate
+  n   <- pars$mat_siz 
+  # lower limit of integration
+  L   <- pars$L  
+  # upper limit of integration
+  U   <- pars$U       
+  # bin size
+  h   <- (U - L) / n  
+  # lower boundaries of bins
+  b   <- L + c(0:n) * h             
+  # midpoints of bins
+  y   <- 0.5 * (b[1:n] + b[2:(n + 1)]) 
+  
+  # Fertility matrix
+  Fmat        <- matrix(0, n, n)
+  Fmat[]      <- matrix(fy(y, pars, h), n, n)
+  
+  # Survival vector
+  Smat   <- c()
+  Smat   <- sx(y, pars)
+  
+  # Growth matrix
+  Gmat   <- matrix(0, n, n)
+  Gmat[] <- t(outer(y, y, gxy, pars)) * h
+  
+  # Growth/survival transition matrix
+  Tmat   <- matrix(0, n, n)
+  
+  # Correct for eviction of offspring
+  for(i in 1:(n / 2)) {
+    Gmat[1,i] <- Gmat[1,i] + 1 - sum(Gmat[,i])
+    Tmat[,i]  <- Gmat[,i] * Smat[i]
+  }
+  
+  # Correct eviction of large adults
+  for(i in (n / 2 + 1):n) {
+    Gmat[n,i] <- Gmat[n,i] + 1 - sum(Gmat[,i])
+    Tmat[,i]  <- Gmat[,i] * Smat[i]
+  }
+  
+  # Full Kernel is simply a summation of fertility and transition matrices
+  k_yx <- Fmat + Tmat
+  
+  return(list(k_yx    = k_yx,
+              Fmat    = Fmat,
+              Tmat    = Tmat,
+              Gmat    = Gmat,
+              meshpts = y))
+}
+
+lambda_ipm <- function(i) {
+  return(Re(eigen(kernel(i)$k_yx)$value[1]))
+}
+
+# mean population growth rate
+lam_mean <- lambda_ipm(pars)
+lam_mean
+
+# observed population growth rate
+# Population counts at time t0
+pop_counts_t0 <- df %>%
+  group_by(year, quad) %>%
+  summarize(n_t0 = n()) %>% 
+  ungroup %>% 
+  mutate(year = year + 1)
+
+# Population counts at time t1
+pop_counts_t1 <- df %>%
+  group_by(year, quad) %>%
+  summarize(n_t1 = n()) %>% 
+  ungroup 
+
+# Calculate observed population growth rates, 
+ # accounting for discontinued sampling!
+pop_counts <- left_join(pop_counts_t0, 
+                        pop_counts_t1) %>% 
+  # by dropping NAs, we remove gaps in sampling!
+  drop_na %>% 
+  group_by(year) %>% 
+  summarise(n_t0 = sum(n_t0),
+            n_t1 = sum(n_t1)) %>% 
+  ungroup %>% 
+  mutate(obs_pgr = n_t1 / n_t0)
+
+# Geometric mean of yearly population growth rates
+lam_mean_count <- exp(mean(log(pop_counts$obs_pgr), na.rm = T))
+
+# Overall (aggregated) population growth rate
+lam_mean_overall <- sum(pop_counts$n_t1) / sum(pop_counts$n_t0)
+
+
+# Building the IPM with ipmr ---------------------------------------------------
+proto_ipm_p <- init_ipm(sim_gen   = "simple",
+                        di_dd     = "di",
+                        det_stoch = "det") %>% 
+  define_kernel(
+    name      = "P",
+    family    = "CC",
+    formula   = s * g,
+    # !!! Check !!! ####
+    s         = plogis(surv_b0 + 
+                          surv_b1 * size_1), 
+    g         = dnorm(size_2, mu_g, grow_sig),
+    # !!! Check !!! ####
+    mu_g      = grow_b0 + grow_b1 * size_1,
+    grow_sig  = sqrt(a * exp(b * size_1)),
+    data_list = pars,
+    states    = list(c('size')),
+    evict_cor = TRUE,
+    evict_fun = truncated_distributions(fun = 'norm', target = 'g')
+  ) %>% 
+  
+  define_kernel(
+    name      = 'F',
+    family    = 'CC',
+    formula   = fecu_b0 * r_d,
+    r_d       = dnorm(size_2, recr_sz, recr_sd),
+    data_list = pars,
+    states    = list(c('size')),
+    evict_cor = TRUE,
+    evict_fun = truncated_distributions("norm", "r_d")
+  ) %>% 
+  
+  define_impl(
+    make_impl_args_list(
+      kernel_names = c(  "P", "F"),
+      int_rule     = rep("midpoint", 2),
+      state_start  = rep("size", 2),
+      state_end    = rep("size", 2))
+  ) %>%
+  
+  define_domains(
+    size = c(pars$L,
+             pars$U,
+             pars$mat_siz)
+  ) %>%
+  
+  define_pop_state(
+    n_size = rep(1 / 200, 200)
+  )
+
+ipmr_p <- make_ipm(proto_ipm  = proto_ipm_p, 
+                   iterations = 200)
+
+lam_mean_ipmr <- lambda(ipmr_p)
+plot(ipmr_p)
+
+lam_out       <- data.frame(coefficient = names(lam_mean_ipmr), 
+                            value       = lam_mean_ipmr)
+write.csv(lam_out, 
+          paste0("adler_2007_ks/data/", sp_abb, "/lambda_vec.csv"), 
+          row.names = F)
+
+lam_out_wide  <- as.list(pivot_wider(lam_out, 
+                                     names_from  = "coefficient", 
+                                     values_from = "value"))
+write.csv(lam_out_wide, 
+          paste0("adler_2007_ks/data/", sp_abb, "/lambda.csv"), 
+          row.names = F)

3dae81d256ebb13bd3c8126ba4d44c73f1021a81
Compagnoni <ac22qawo@usr.idiv.de>
2024-10-22 23:53:32 +0200

    Load packages, install and load them if not installed.

---
+++ helper_functions/load_packages.R
@@ -0,0 +1,21 @@
+
+# load or install packages 
+load_packages <- function( ... ){
+  
+  # Define CRAN packages
+  .cran_packages <- substitute( alist(...) ) %>% 
+                      eval( ) %>% 
+                      sapply( deparse )
+  
+  # Check if CRAN packages are installed
+  .inst <- .cran_packages %in% installed.packages() 
+  
+  if(any(!.inst)) {
+    # Install missing CRAN packages
+    install.packages(.cran_packages[!.inst]) 
+  }
+  
+  # Load required packages
+  sapply(.cran_packages, require, character.only = TRUE) 
+  
+}

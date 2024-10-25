f398687b0042d1ef2671bef88fcea6e97d3ee6a5
Compagnoni <ac22qawo@usr.idiv.de>
2024-10-25 15:50:32 +0200

    Run models for all species simultaneously

---
+++ adler_2007_ks/R/wrapper_mean_models.R
@@ -0,0 +1,36 @@
+# run all plant tracker files, all at once
+library(tidyverse)
+
+# Function to quote bare names for tidy evaluation
+quote_bare <- function(...){
+  substitute(alist(...)) %>% 
+    eval() %>% 
+    sapply(deparse)  
+}
+
+# Set up working directory and species codes
+dir         <- 'adler_2007_ks/R/'
+spp_codes_v <- quote_bare( ange, arlo, bocu, bogr, 
+                           buda, pavi, scsc, spas, spcr )
+
+
+# function to run mean models (models with all data comprised)
+run_mean_models <- function( spp_code_x ){
+  print( spp_code_x )
+  source( paste0(dir, spp_code_x, '/',
+                 'ipm_kansas_',spp_code_x,'.R') )
+}
+
+# Run plant tracker for all species
+lapply( spp_codes_v, run_mean_models )
+
+
+# # function to run year-specific IPMs
+# run_yearly_ipms <- function( spp_code_x ){
+#   print( spp_code_x )
+#   source( paste0(dir, spp_code_x, '/',
+#                  'ipm_kansas_',spp_code_x,'_year.R') )
+# }
+# 
+# # Run plant tracker for all species
+# lapply( spp_codes_v[6:9], run_yearly_ipms )

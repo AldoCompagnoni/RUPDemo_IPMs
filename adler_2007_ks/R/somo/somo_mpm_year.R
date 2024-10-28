4c16dbe808248c6ab18b31d60cd62776f8900200
NeisseN <neisse.n@protonmail.com>
2024-10-28 13:18:57 +0100

    create mpm's not perfect yet

--- adler_2007_ks/R/somo/somo_mpm_year.R
+++ adler_2007_ks/R/somo/somo_mpm_year.R
@@ -1,8 +1,9 @@
-# Example of how to construct an MPM from chart quadrat data
+# MPM year specific Adler 2007 Kansas Solidago mollis
 
-# Author: Aldo Compagnoni
-# Email: aldo.compagnoni@gmail.com
-# Date: 2024.10.23
+# Author: Niklas Neisse
+# Email: neisse.n@protonmail.com &
+#  aldo.compagnoni@gmail.com
+# Date: 2024.10.28
 
 # Original models from https://doi.org/10.1111/j.1365-2745.2009.01585.x
 #   check the appendices for details on model fitting!
@@ -17,19 +18,22 @@ load_packages( tidyverse, patchwork, skimr, lme4, ggthemes )
 # Data -------------------------------------------------------------------------
 
 # Define the species variable
-species <- 'Psoralea tenuiflora'
-sp_abb  <- 'pste'
+species <- 'Solidago mollis'
+sp_abb  <- tolower(
+  gsub(" ", "", paste(substr(unlist(strsplit(species, " ")), 1, 2), 
+                      collapse = "")))
 
 # Species data frame
-df <- read.csv( 'adler_2007_ks/data/pste/ks_pste.csv' ) %>% 
-        filter(Species == species) %>%
-        select( -c(Suspect, nearEdge, Site) ) %>% # geometry, 
-        mutate(across(c(Quad), as.factor)) %>% 
-        rename(species  = Species, 
-               survives = survives_tplus1,
-               year     = Year,
-               quad     = Quad,
-               track_id = trackID)
+df <- read.csv(paste0('adler_2007_ks/data/', 
+                      sp_abb, '/ks_', sp_abb, '.csv')) %>% 
+  filter(Species == species) %>%
+  select( -c(Suspect, nearEdge, Site) ) %>% # geometry, 
+  mutate(across(c(Quad), as.factor)) %>% 
+  rename(species  = Species, 
+         survives = survives_tplus1,
+         year     = Year,
+         quad     = Quad,
+         track_id = trackID)
 
 # first look at the data
 skim(df)
@@ -54,6 +58,12 @@ inv_plot_per_year <- df %>%
 
 inv_plot_per_year
 
+
+# Create output directory if it doesn't exist
+if (!dir.exists(paste0("adler_2007_ks/results/", sp_abb))) {
+  dir.create(paste0("adler_2007_ks/results/", sp_abb))
+}
+
 ggsave(paste0("adler_2007_ks/results/", 
               sp_abb, 
               "/overall_inventory_quadrat_per_year.png"),  
@@ -65,43 +75,43 @@ ggsave(paste0("adler_2007_ks/results/",
 
 # Survival data that retains all ages
 surv_out_df <- df %>% 
-                subset( !is.na(survives) ) %>%
-                select(quad, track_id, year, age, survives ) 
+  subset( !is.na(survives) ) %>%
+  select(quad, track_id, year, age, survives ) 
 
 # Total number of individuals time t0
 indiv_t0  <- df %>%  
-              count(quad, year) %>% 
-              mutate( year = as.integer(year) ) %>% 
-              mutate( year = year + 1 ) %>% 
-              rename( parents = n )
+  count(quad, year) %>% 
+  mutate( year = as.integer(year) ) %>% 
+  mutate( year = year + 1 ) %>% 
+  rename( parents = n )
 
 # cases for quadrats and year
 cases_df  <- df %>% 
-              dplyr::select( quad, year ) %>% 
-              mutate( year = as.integer(year) ) %>% 
-              unique
+  dplyr::select( quad, year ) %>% 
+  mutate( year = as.integer(year) ) %>% 
+  unique
 
 # Total number of recruits at time t1
 indiv_t1 <- df %>%  
-              subset( recruit == 1 ) %>% 
-              count(quad, year) %>% 
-              mutate( year = as.integer(year) ) %>% 
-              rename( recruits = n ) %>% 
-              # include all available quad X year combinations
-              right_join( cases_df ) %>% 
-              # if is.na(recruits), then recruits == 0
-              mutate( recruits = replace( recruits,
-                                          is.na(recruits),
-                                          0) )
+  subset( recruit == 1 ) %>% 
+  count(quad, year) %>% 
+  mutate( year = as.integer(year) ) %>% 
+  rename( recruits = n ) %>% 
+  # include all available quad X year combinations
+  right_join( cases_df ) %>% 
+  # if is.na(recruits), then recruits == 0
+  mutate( recruits = replace( recruits,
+                              is.na(recruits),
+                              0) )
 
 # recruits data frame
 recr_df  <- left_join( indiv_t0, indiv_t1 ) %>% 
-              # change this, so that it is year_t0 instead of year_t1
-              mutate( year = year - 1 ) %>% 
-              mutate( year = as.character(year) ) %>% 
-              # calculate per capita recruitment. 
-              #   Support: 0 to infinity.
-              mutate( pcr = recruits / parents ) 
+  # change this, so that it is year_t0 instead of year_t1
+  mutate( year = year - 1 ) %>% 
+  mutate( year = as.character(year) ) %>% 
+  # calculate per capita recruitment. 
+  #   Support: 0 to infinity.
+  mutate( pcr = recruits / parents ) 
 
 # Create the data folder for the species if it does not exits already
 if (!dir.exists(paste0("adler_2007_ks/data/", sp_abb))) {
@@ -118,11 +128,11 @@ write.csv(recr_df,     paste0("adler_2007_ks/data/", sp_abb, "/recruitment_df.cs
 
 # survival data. For this model, only two age classes
 surv_df <- surv_out_df %>% 
-                # consider all individuals past year 0 as if they had age 1
-                mutate( age = dplyr::case_when(
-                                age == 0 ~ 0,
-                                age > 0  ~ 1 ) 
-                      )
+  # consider all individuals past year 0 as if they had age 1
+  mutate( age = dplyr::case_when(
+    age == 0 ~ 0,
+    age > 0  ~ 1 ) 
+  )
 
 # Survival model: Random effect of year only
 mod_surv   <- glmer( survives ~ age + (1 | year) + (1 | quad),
@@ -133,45 +143,45 @@ mod_surv   <- glmer( survives ~ age + (1 | year) + (1 | quad),
 shat0       <- data.frame( age  = 0,
                            year = coef(mod_surv)$year %>% rownames,
                            stringsAsFactors = F ) %>%
-                  mutate( shat = coef(mod_surv)$year[,1] %>% boot::inv.logit() )
+  mutate( shat = coef(mod_surv)$year[,1] %>% boot::inv.logit() )
 shat1       <- data.frame( age  = 1,
                            year = coef(mod_surv)$year %>% rownames,
                            stringsAsFactors = F ) %>%
-                  mutate( shat = coef(mod_surv)$year[,1:2] %>% 
-                            rowSums %>% 
-                            boot::inv.logit() )
+  mutate( shat = coef(mod_surv)$year[,1:2] %>% 
+            rowSums %>% 
+            boot::inv.logit() )
 
 # Hmmm, I realize now this is a sub-optimal object name...
 shat_df     <- bind_rows( shat0, shat1 )
 
 # plot raw data against model predictions
 p_surv <- surv_df %>%
-            subset( !is.na(age) ) %>%
-            group_by( year, age ) %>%
-            summarise( surv_p = sum(survives) / n() ) %>%
-            ungroup %>%
-            mutate( year = as.character(year) ) %>% 
-            left_join( shat_df ) %>% 
-            mutate( age = as.factor(age) ) %>% 
-            pivot_longer( surv_p:shat, 
-                          names_to  = 'Measure',
-                          values_to = 'Survival' ) %>% 
-            mutate( Measure = dplyr::case_when(
-                          Measure == 'shat'   ~ 'Predicted',
-                          Measure == 'surv_p' ~ 'Observed' ) 
-                    ) %>% 
-            ggplot() +
-            geom_point( aes(x = age, 
-                            y = Survival,
-                            color = Measure),
-                        alpha = 0.75 ) +
-            theme_bw() +
-            facet_wrap( ~ year, ncol = 8 ) +
-            lims( y = c(0,1) ) +
-            labs( x = 'Age',
-                  y = 'Survival' ) +
-            theme( legend.position = 'top' ) +
-            scale_color_colorblind()
+  subset( !is.na(age) ) %>%
+  group_by( year, age ) %>%
+  summarise( surv_p = sum(survives) / n() ) %>%
+  ungroup %>%
+  mutate( year = as.character(year) ) %>% 
+  left_join( shat_df ) %>% 
+  mutate( age = as.factor(age) ) %>% 
+  pivot_longer( surv_p:shat, 
+                names_to  = 'Measure',
+                values_to = 'Survival' ) %>% 
+  mutate( Measure = dplyr::case_when(
+    Measure == 'shat'   ~ 'Predicted',
+    Measure == 'surv_p' ~ 'Observed' ) 
+  ) %>% 
+  ggplot() +
+  geom_point( aes(x = age, 
+                  y = Survival,
+                  color = Measure),
+              alpha = 0.75 ) +
+  theme_bw() +
+  facet_wrap( ~ year, ncol = 8 ) +
+  lims( y = c(0,1) ) +
+  labs( x = 'Age',
+        y = 'Survival' ) +
+  theme( legend.position = 'top' ) +
+  scale_color_colorblind()
 
 # store the plot
 ggsave(paste0("adler_2007_ks/results/", 
@@ -187,9 +197,9 @@ ggsave(paste0("adler_2007_ks/results/",
 
 # 1. yes or no recruits?
 yesno_df <- recr_df %>% 
-              mutate( recruits_yesno = as.numeric(recruits > 0) ) %>% 
-              dplyr::select( quad, year, parents, recruits_yesno ) %>% 
-              drop_na
+  mutate( recruits_yesno = as.numeric(recruits > 0) ) %>% 
+  dplyr::select( quad, year, parents, recruits_yesno ) %>% 
+  drop_na
 
 # Yes-or-no recruits? (NOTE: parent number matters)
 yesno_mod    <- glmer( recruits_yesno ~ (1 | year),
@@ -199,10 +209,10 @@ yesno_mod    <- glmer( recruits_yesno ~ (1 | year),
 # predict zeroes
 yesno_pred_df <- expand.grid( year    = coef(yesno_mod)$year %>% rownames,
                               stringsAsFactors = F ) %>% 
-                    mutate( yesno_prob = predict( yesno_mod, 
-                                                  newdata = .,
-                                                  type = 'response' ) 
-                    ) 
+  mutate( yesno_prob = predict( yesno_mod, 
+                                newdata = .,
+                                type = 'response' ) 
+  ) 
 
 
 # 2. per capita recruitment **conditional** on recruitment happening
@@ -216,32 +226,32 @@ pcr_mod      <- glmer( pcr ~ (1 | year),
 # predict per capita recruitment
 pcr_pred_df  <- expand.grid( year    = coef(pcr_mod)$year %>% rownames,
                              stringsAsFactors = F ) %>% 
-                  # conditional (on recruitment == 1) per capita recruitment
-                  mutate( cond_pcr_hat = predict( pcr_mod, 
-                                                  newdata = .,
-                                                  type = 'response' ) 
-                  )
+  # conditional (on recruitment == 1) per capita recruitment
+  mutate( cond_pcr_hat = predict( pcr_mod, 
+                                  newdata = .,
+                                  type = 'response' ) 
+  )
 
 # hurdle model predictions
 recr_pred_df <- left_join( yesno_pred_df, 
                            pcr_pred_df ) %>% 
-                  mutate( pcr_hat = cond_pcr_hat * yesno_prob ) 
+  mutate( pcr_hat = cond_pcr_hat * yesno_prob ) 
 
 # plot on recruitment
 recr_p <- recr_df %>% 
-            left_join( recr_pred_df ) %>% 
-            mutate( recr_hat = pcr_hat * parents ) %>% 
-            subset( year > 38 & year < 72 ) %>% 
-            ggplot() + 
-            geom_point( aes( x = parents,
-                             y = recruits) ) +
-            geom_point( aes( x = parents,
-                             y = recr_hat),
-                        color = 'red' ) +
-            geom_abline( intercept = 0,
-                         slope     = 1 ) +
-            facet_wrap( ~ year, ncol = 4 ) +
-            theme_bw()
+  left_join( recr_pred_df ) %>% 
+  mutate( recr_hat = pcr_hat * parents ) %>% 
+  subset( year > 38 & year < 72 ) %>% 
+  ggplot() + 
+  geom_point( aes( x = parents,
+                   y = recruits) ) +
+  geom_point( aes( x = parents,
+                   y = recr_hat),
+              color = 'red' ) +
+  geom_abline( intercept = 0,
+               slope     = 1 ) +
+  facet_wrap( ~ year, ncol = 4 ) +
+  theme_bw()
 
 # store the plot
 ggsave(paste0("adler_2007_ks/results/", 
@@ -268,12 +278,12 @@ recr_df %>%
 
 # MPM parameters
 mpm_df <- shat_df %>% 
-              pivot_wider( names_from  = 'age',
-                           values_from = 'shat' ) %>% 
-              left_join( dplyr::select( recr_pred_df,
-                                        year, pcr_hat) ) %>% 
-              drop_na %>% 
-              ungroup
+  pivot_wider( names_from  = 'age',
+               values_from = 'shat' ) %>% 
+  left_join( dplyr::select( recr_pred_df,
+                            year, pcr_hat) ) %>% 
+  drop_na %>% 
+  ungroup
 
 # lambda function
 lambda_mpm <- function(x) x %>% eigen %>% .$value %>% Re %>% max
@@ -319,74 +329,74 @@ make_mat <- function( year_x, mat_df ){
 
 # project based on stage distribution
 stage_counts <- df %>%
-                  mutate( year = as.character(year) ) %>% 
-                  mutate( age = dplyr::case_when(
-                          age == 0 ~ 0,
-                          age > 0  ~ 1 ) 
-                          ) %>% 
-                  group_by(year, age ) %>%
-                  summarize(n_t0 = n()) %>% 
-                  ungroup %>% 
-                  # only retain year_t0 for which we have data
-                  right_join( dplyr::select(mpm_df,year) ) %>% 
-                  drop_na %>% 
-                  pivot_wider( names_from  = 'age',
-                               values_from = 'n_t0',
-                               values_fill = 0 ) %>% 
-                  drop_na %>% 
-                  mutate( year = as.character(year) )
-  
+  mutate( year = as.character(year) ) %>% 
+  mutate( age = dplyr::case_when(
+    age == 0 ~ 0,
+    age > 0  ~ 1 ) 
+  ) %>% 
+  group_by(year, age ) %>%
+  summarize(n_t0 = n()) %>% 
+  ungroup %>% 
+  # only retain year_t0 for which we have data
+  right_join( dplyr::select(mpm_df,year) ) %>% 
+  drop_na %>% 
+  pivot_wider( names_from  = 'age',
+               values_from = 'n_t0',
+               values_fill = 0 ) %>% 
+  drop_na %>% 
+  mutate( year = as.character(year) )
+
 # projected lambda (takes into account observed stage distribution)
 proj_lam <- function( year_x, mat_df ){
   
   # year-specific stage counts
   tmp_stage <- stage_counts %>% 
-                subset( year == year_x ) %>% 
-                .[,c('0','1')]
+    subset( year == year_x ) %>% 
+    .[,c('0','1')]
   
   # year-specific projection matrix
   tmp_mat   <- make_mat( year_x, mat_df )
   
   nt0 <- sum(tmp_stage)
   nt1 <- ( tmp_mat %*% as.numeric(tmp_stage) ) %>% sum
-
+  
   nt1 / nt0
   
 }
 
 # model lambdas
 lam_df        <- mpm_df %>% 
-                  mutate( lam      = sapply( year, year_lam, mpm_df),
-                          proj_lam = sapply( year, proj_lam, mpm_df) )
-                
+  mutate( lam      = sapply( year, year_lam, mpm_df),
+          proj_lam = sapply( year, proj_lam, mpm_df) )
+
 # Population counts at time t0
 pop_counts_t0 <- df %>%
-                  group_by(year, quad) %>%
-                  summarize(n_t0 = n()) %>% 
-                  ungroup %>% 
-                  mutate(year = year + 1)
+  group_by(year, quad) %>%
+  summarize(n_t0 = n()) %>% 
+  ungroup %>% 
+  mutate(year = year + 1)
 
 # Population counts at time t1
 pop_counts_t1 <- df %>%
-                  group_by(year, quad ) %>%
-                  summarize(n_t1 = n()) %>% 
-                  ungroup 
+  group_by(year, quad ) %>%
+  summarize(n_t1 = n()) %>% 
+  ungroup 
 
 # Calculate observed population growth rates, 
 #   accounting for discontinued sampling!
 pop_counts <- left_join(pop_counts_t0, 
                         pop_counts_t1) %>% 
-                # by dropping NAs, we remove gaps in sampling!
-                drop_na %>% 
-                group_by(year) %>% 
-                summarise( n_t0 = sum(n_t0),
-                           n_t1 = sum(n_t1) ) %>% 
-                ungroup %>% 
-                mutate( obs_pgr = n_t1 / n_t0) %>% 
-                mutate( year = as.character(year) ) %>% 
-                left_join( mpm_df ) %>% 
-                mutate( year = as.character(year) ) %>% 
-                left_join( lam_df )
+  # by dropping NAs, we remove gaps in sampling!
+  drop_na %>% 
+  group_by(year) %>% 
+  summarise( n_t0 = sum(n_t0),
+             n_t1 = sum(n_t1) ) %>% 
+  ungroup %>% 
+  mutate( obs_pgr = n_t1 / n_t0) %>% 
+  mutate( year = as.character(year) ) %>% 
+  left_join( mpm_df ) %>% 
+  mutate( year = as.character(year) ) %>% 
+  left_join( lam_df )
 
 # mostly over-predicted population growth
 #   not as bad as the published data though (see below)!
@@ -408,8 +418,8 @@ mpm_df %>%
               'per_capita_recruitment',
               'SpeciesAuthor') ) %>% 
   write.csv( paste0("adler_2007_ks/data/", 
-                     sp_abb, 
-                     "/mpm_pste.csv"),  
+                    sp_abb, 
+                    "/mpm_pste.csv"),  
              row.names = F )
 
 
@@ -421,15 +431,15 @@ load( 'adler_2007_ks/data/COMPADRE_v.6.23.5.0.RData' )
 
 # identify the matrices across COMPADRE 
 id <- compadre$metadata %>% 
-        subset( grepl('Psoralea', SpeciesAuthor ) ) %>% 
-        subset( MatrixComposite == 'Individual' ) %>% 
-        subset( MatrixStartYear %in% paste0('19', pop_counts$year) ) %>% 
-        row.names %>% 
-        as.numeric
+  subset( grepl('Psoralea', SpeciesAuthor ) ) %>% 
+  subset( MatrixComposite == 'Individual' ) %>% 
+  subset( MatrixStartYear %in% paste0('19', pop_counts$year) ) %>% 
+  row.names %>% 
+  as.numeric
 
 # years we can directly compare
 yrs_mat   <- compadre$metadata$MatrixStartYear[id] %>% 
-              gsub('19','',.)
+  gsub('19','',.)
 
 # compadre's matrices
 comp_mats <- compadre$mat[id] %>% setNames( yrs_mat )
@@ -440,7 +450,7 @@ pub_mat_df   <- data.frame( year = yrs_mat ) %>%
   mutate( `0`     = sapply( yrs_mat, function(x) pluck(comp_mats, x)$matA[2,1] ),
           `1`     = sapply( yrs_mat, function(x) pluck(comp_mats, x)$matA[2,2] ),
           pcr_hat = sapply( yrs_mat, function(x) pluck(comp_mats, x)$matA[1,1] )
-          ) %>% 
+  ) %>% 
   # calculate lambda and projected lambda
   mutate( lam      = sapply( year, year_lam, .),
           proj_lam = sapply( year, proj_lam, .) ) %>% 
@@ -451,8 +461,8 @@ pub_mat_df   <- data.frame( year = yrs_mat ) %>%
 
 # compare population growth rates across estimations
 compare_df <- pop_counts %>% 
-                left_join( pub_mat_df ) %>% 
-                drop_na
+  left_join( pub_mat_df ) %>% 
+  drop_na
 
 # Recruitment: our estimation vs. publication
 ggplot( compare_df ) +
@@ -497,10 +507,10 @@ rmse <- function( target, yhat ) (target - yhat)^2 %>% mean %>% sqrt
 
 # data frame to ease computation
 compute_df <- compare_df %>% 
-                dplyr::select( obs_pgr, pub_lam, pub_proj_lam, 
-                               lam, proj_lam ) %>% 
-                as.data.frame %>% 
-                drop_na
+  dplyr::select( obs_pgr, pub_lam, pub_proj_lam, 
+                 lam, proj_lam ) %>% 
+  as.data.frame %>% 
+  drop_na
 
 # dramatically better fit than published lambdas
 rmse( compute_df$obs_pgr,
---
+++ adler_2007_ks/R/somo/somo_tracker.R
@@ -0,0 +1,99 @@
+# plantTracker for Adler 2007 Kansas Solidago mollis
+
+# Author: Niklas Neisse
+# Email: neisse.n@protonmail.com
+# Date: 2024.10.28
+
+# Code adapted from: https://github.com/aestears/plantTracker
+# Adapted from plantTracker How to (Stears et al. 2022)
+
+# load packages
+source( 'helper_functions/load_packages.R' )
+load_packages( sf, plantTracker )
+
+
+# Define directories for data
+data_dir <- 'adler_2007_ks/data/'
+quad_dir <- paste0( data_dir, 'quadrat_data/')
+sh_dir   <- paste0( quad_dir, 'arcexport/')          
+
+# Function to quote bare names for tidy evaluation
+quote_bare <- function(...){
+  # creates a list of the arguments, but keeps their expressions unevaluated
+  # wraps the list, original expressions (the bare names) without evaluation
+  substitute(alist(...)) %>% 
+    # evaluates the substituted list, which results in a list of the bare name
+    eval() %>% 
+    # converts the list into their character representations
+    # apply to each element, returning a character vector of the names
+    sapply(deparse)  
+}
+
+# Read species list and filter for target species
+forb_list     <- read.csv(paste0(data_dir,
+                               "quadrat_data/species_list.csv"))  %>% 
+  dplyr::arrange(desc(count)) %>% 
+  filter(type == 'forb') %>% 
+  head(25)
+
+# Select the x_th species (target species)
+target_spec <- forb_list %>% .[c(3),]  
+
+# Define the species variable and abbreviation
+species <- target_spec$species
+sp_abb  <- tolower(gsub(" ", "", paste(substr(
+  strsplit(species, " ")[[1]], 1, 2), collapse = "")))
+
+# Read in quadrat inventory data and prepare for plantTracker
+quad_inv        <- read.csv(paste0(data_dir,
+                                   "quadrat_data/quadrat_inventory.csv"), 
+                            sep=',') %>% 
+  dplyr::select(-year) %>% 
+  as.list
+
+# Remove NAs
+inv_ks          <- lapply(X = quad_inv, 
+                          FUN = function(x) x[is.na(x) == FALSE])
+
+# Replace dots in names with dashes
+names(inv_ks)   <- gsub( '\\.','-',names(inv_ks) )  
+
+# Read spatial data (polygon for each species per quadrat)
+dat             <- readRDS(file=paste0(quad_dir,
+                                       "KS_polygons_full.rds"))
+
+# Subset data for the target species
+dat_target_spec <- dat[dat$SCI_NAME %in% target_spec$species,] %>%
+  # Rename columns
+  setNames( quote_bare(Species, Site, 
+                       Quad, Year, geometry) )  
+
+# Prepare data for the trackSpp function
+datTrackSpp <- trackSpp(dat_target_spec, 
+                        inv_ks,
+                        # Number of years in dormancy
+                        dorm         = 0,
+                        # Buffer size
+                        buff         = 5,
+                        # Allow for clonal tracking
+                        clonal       = FALSE,
+                        # Buffer for genet
+                        buffGenet    = 5,
+                        # Aggregate by genet
+                        aggByGenet   = TRUE,
+                        # Flag potential issues
+                        flagSuspects = TRUE)   
+
+# Create output directory if it doesn't exist
+if (!dir.exists(paste0("adler_2007_ks/data/", sp_abb))) {
+  dir.create(paste0("adler_2007_ks/data/", sp_abb))
+}
+
+# Save the tracked data to a CSV file
+datTrackSpp %>% 
+  as.data.frame %>% 
+  dplyr::select(Site, Quad, Species, trackID, Year, basalArea_genet, recruit,
+                survives_tplus1, age, size_tplus1, nearEdge, Suspect) %>%
+  write.csv(paste0('adler_2007_ks/data/', sp_abb, '/ks_', sp_abb, '.csv'), 
+            row.names = FALSE)  
+

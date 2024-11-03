8e798dcea0625043c9c8f5104fe156769d73f78c
NeisseN <neisse.n@protonmail.com>
2024-11-03 20:17:23 +0100

    improve folder structure and mpm's

--- adler_2007_ks/R/heni_mpm/heni_tracker.R
+++ adler_2007_ks/R/heni_mpm/heni_tracker.R
@@ -1,4 +1,4 @@
-# plantTracker for Adler 2007 Kansas Thelesperma megapotamicum
+# plantTracker for Adler 2007 Kansas Hedyotis nigricans
 
 # Author: Niklas Neisse
 # Email: neisse.n@protonmail.com
@@ -11,11 +11,13 @@
 source( 'helper_functions/load_packages.R' )
 load_packages( sf, plantTracker, tidyverse )
 
+# Type of population model
+mod_type <- 'mpm'
 
-# Define directories for data
-data_dir <- 'adler_2007_ks/data/'
-quad_dir <- paste0( data_dir, 'quadrat_data/')
-sh_dir   <- paste0( quad_dir, 'arcexport/')          
+# Directories 1
+data0_dir <- 'adler_2007_ks/data/'
+quad_dir  <- paste0( data0_dir, 'quadrat_data/')
+sh_dir    <- paste0( quad_dir,  'arcexport/')          
 
 # Function to quote bare names for tidy evaluation
 quote_bare <- function(...){
@@ -30,22 +32,28 @@ quote_bare <- function(...){
 }
 
 # Read species list and filter for target species
-forb_list     <- read.csv(paste0(data_dir,
-                               "quadrat_data/species_list.csv"))  %>% 
-  arrange(desc(count)) %>% 
+forb_list     <- read.csv(paste0(data0_dir,
+                                 "quadrat_data/species_list.csv"))  %>% 
+  dplyr::arrange(desc(count)) %>% 
   filter(type == 'forb') %>% 
   head(25)
 
 # Select the x_th species (target species)
-target_spec <- forb_list %>% .[c(10),]  
+target_spec <- forb_list %>% .[c(8),]  
 
 # Define the species variable and abbreviation
 species <- target_spec$species
 sp_abb  <- tolower(gsub(" ", "", paste(substr(
   strsplit(species, " ")[[1]], 1, 2), collapse = "")))
 
+# Directories 2
+data_dir   <- paste0('adler_2007_ks/data/',    sp_abb, '_', mod_type)
+result_dir <- paste0('adler_2007_ks/results/', sp_abb, '_', mod_type)
+R_dir      <- paste0('adler_2007_ks/R/',       sp_abb, '_', mod_type)
+
+
 # Read in quadrat inventory data and prepare for plantTracker
-quad_inv        <- read.csv(paste0(data_dir,
+quad_inv        <- read.csv(paste0(data0_dir,
                                    "quadrat_data/quadrat_inventory.csv"), 
                             sep=',') %>% 
   dplyr::select(-year) %>% 
@@ -85,8 +93,8 @@ datTrackSpp <- trackSpp(dat_target_spec,
                         flagSuspects = TRUE)   
 
 # Create output directory if it doesn't exist
-if (!dir.exists(paste0("adler_2007_ks/data/", sp_abb))) {
-  dir.create(paste0("adler_2007_ks/data/", sp_abb))
+if (!dir.exists(data_dir)) {
+  dir.create(data_dir)
 }
 
 # Save the tracked data to a CSV file
@@ -94,5 +102,5 @@ datTrackSpp %>%
   as.data.frame %>% 
   dplyr::select(Site, Quad, Species, trackID, Year, basalArea_genet, recruit,
                 survives_tplus1, age, size_tplus1, nearEdge, Suspect) %>%
-  write.csv(paste0('adler_2007_ks/data/', sp_abb, '/ks_', sp_abb, '.csv'), 
+  write.csv(paste0(data_dir, '/ks_', sp_abb, '.csv'), 
             row.names = FALSE)  

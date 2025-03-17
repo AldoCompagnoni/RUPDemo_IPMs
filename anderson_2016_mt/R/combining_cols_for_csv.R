a98505b2b43376d109e13a9c1263061f5a828e1a
Compagnoni <ac22qawo@usr.idiv.de>
2025-03-17 14:46:55 +0100

    Check whether spplist has been updated; if not, update and replace.

--- anderson_2016_mt/R/combining_cols_for_csv.R
+++ anderson_2016_mt/R/combining_cols_for_csv.R
@@ -1,9 +1,20 @@
-data = read.csv("anderson_2016_mt/data/quadrat_data/species_list.csv")
-data = data[,c(2:6)]
-head(data)
-data <- data %>%
-  mutate(species = paste(species, X, sep = " "))
-data = data[,c(1,3:6)]
-head(data)
+# Combine species binomial in a single column 
+library(tidyverse)
 
-write.csv(data, "anderson_2016_mt/data/quadrat_data/species_list.csv", row.names = F)
+# read in species list
+spp_l <- read.csv("anderson_2016_mt/data/quadrat_data/species_list.csv")
+
+# check whether the species list has already been changed.
+#   in that case, column "X" will not be present
+if( "X" %in% names(spp_l) ){
+  
+  # create species column containing entire species binomial
+  spp_l %>%
+    mutate(species = paste(species, X, sep = " ") ) %>% 
+    # remove column X (now contained in "species")
+    dplyr::select(-X) %>% 
+    # replace original file
+    write.csv("anderson_2016_mt/data/quadrat_data/species_list.csv", 
+              row.names = F)  
+}
+

852ef34aa7c9841f2e53e5a5872b35016b760e2a
DianaSpurite <diana.spurite@posteo.de>
2025-04-16 12:05:36 +0200

    checking IPM's and adding pardino script

--- anderson_2016_mt/R/bogr/anderson16mt_bogr_padrino.R
+++ anderson_2016_mt/R/bogr/anderson16mt_bogr_padrino.R
@@ -1,13 +1,13 @@
-# Populating padrino - Adler 2007 Kansas - Bouteloua gracilis
+# Populating padrino - Anderson 2016 Arizona - Allium textile
 
-# Author: Niklas Neisse
-# Co    : Aspen Workman, Aldo Compagnoni
-# Email : neisse.n@protonmail.com
+# Author: Diana Spurite
+# Co    : Aspen Workman, Aldo Compagnoni, Niklas Neisse
+# Email : diana.spurite@posteo.de
 # Main  : aldo.compagnoni@idiv.de
 # Web   : https://aldocompagnoni.weebly.com/
-# Date  : 2025.01.13
+# Date  : 2025.03.18
 
-# Publication: https://doi.org/10.1890/0012-9658(2007)88[2673:LMQFKP]2.0.CO;2
+# Publication: https://doi.org/10.1890/11-0193.1
 
 
 # Comments ---------------------------------------------------------------------
@@ -23,15 +23,15 @@
 
 # Data -------------------------------------------------------------------------
 # Define publication 
-author_year <- 'adler_2007'
+author_year <- 'anderson_2016'
 # Define region abbreviation
-region_abb  <- 'ks'
+region_abb  <- 'mt'
 # Define species 
 species     <- 'Bouteloua gracilis'
 
 # A unique identifier for each model. 
 #  It is 6 alphanumeric characters with no spaces
-ipm_id      <- 'nnn002'
+ipm_id      <- 'bgr164'
 
 # IPM-Type: 'year_specific' or 'mean'?
 ipm_type    <- 'year_specific'
@@ -47,9 +47,9 @@ tax_family <- 'Poaceae'
 # The accepted order
 tax_order  <- 'Poales' 
 # The accepted class
-tax_class  <- 'Liliopsida'
+tax_class  <- 'Magnoliopsida'
 # The accepted phylum
-tax_phylum <- 'Magnoliophyta' 
+tax_phylum <- 'Streptophyta'
 # The kingdom
 kingdom    <- 'Plantae'
 # The type of organism. For plants, this is usually something like 
@@ -67,7 +67,7 @@ angio_gymno   <- 'angio'
 
 # Publication information ------------------------------------------------------
 # The last names of each author on the manuscript, separated by a semicolon
-authors  <- 'Adler; Tyburczy; Laurenroth'
+authors  <- 'Anderson; Vermeire; Adler'
 # The abbreviated name of the journal that the model appears in. 
 #  This follows the BIOSIS format. 
 #  Exceptions are when the source is not a journal 
@@ -76,9 +76,9 @@ authors  <- 'Adler; Tyburczy; Laurenroth'
 #  then include a link in the remark column
 journal  <- 'Ecology'
 #  The year the article was published
-pub_year <- '2007'
+pub_year <- '2016'
 # The DOI of the publication (NOT THE doi.org URL though!!)
-doi      <- '10.1890/0012-9658(2007)88[2673%3ALMQFKP]2.0.CO%3B2'
+doi      <- '10.6084/m9.figshare.c.3304113.v1'
 # The last name of the corresponding author
 corresponding_author <- 'Adler'
 # The corresponding author’s email, along with the year of publication 
@@ -90,12 +90,12 @@ email_year <- 'peter.adler@usu.edu (2024)'
 # Any qualitative comments you may have on the model. 
 #  These can range from comments to accuracy of GPS coordinates to descriptions 
 #  of the different levels of a treatment that was applied
-remark   <- NA
+remark   <- 'cattle grazing treatments with light, moderate, and heavy stocking rates of 1.24, 0.92, and 0.76 ha/animal-unit-month (two pastures in each)'
 # The full APA style citation for the paper
-apa_citation <- 'Adler, P. B., Tyburczy, W. R., & Lauenroth, W. K. (2007). LONG‐TERM MAPPED QUADRATS FROM KANSAS PRAIRIE DEMOGRAPHIC INFORMATION FOR HERBACEOUS PLANTS: Ecological Archives E088‐161. Ecology, 88(10), 2673-2673.'
+apa_citation <- 'Anderson, J., Vermeire, L., & Adler, P. B. (2011). Fourteen years of mapped, permanent quadrats in a northern mixed prairie, USA. Ecology, 92(8), 1703. https://esapubs.org/archive'
 # If there is one, a link to the Electronic Supplementary Material that 
 #  contains further details/parameter values for the model
-demog_appendix_link <- 'https://figshare.com/collections/LONG-TERM_MAPPED_QUADRATS_FROM_KANSAS_PRAIRIE_DEMOGRAPHIC_INFORMATION_FOR_HERBACEOUS_PLANTS/3299993'
+demog_appendix_link <- 'https://figshare.com/collections/Fourteen_years_of_mapped_permanent_quadrats_in_a_northern_mixed_prairie_USA/3304113'
 
 
 # Data collection information --------------------------------------------------
@@ -103,11 +103,11 @@ demog_appendix_link <- 'https://figshare.com/collections/LONG-TERM_MAPPED_QUADRA
 start_year  <- 1932
 # The month of the year that demographic data collection began. 
 #  This is an integer between 1 and 12, where 1 corresponds to January
-start_month <- 6
+start_month <- NA
 #  The final year of demographic data collection. Formatted YYYY
-end_year    <- 1971
+end_year    <- 1945
 # The month of the year that demographic data collection concluded
-end_month   <- 6
+end_month   <- NA
 # Indicates the time step (periodicity) for which the seasonal, annual, 
 #  or multi-annual IPM was constructed. For example, 1 indicates that 
 #  the IPM iteration period is 1 year; 
@@ -125,12 +125,12 @@ population_name <- NA
 number_populations <- NA
 # The decimal latitude of the population. 
 #  Use the dms_deg function from pdbDigitUtils to generate this
-lat         <- '38.8'
+lat         <- '46.4'
 # The decimal longitude of the population. 
 #  Use the dms_deg function from pdbDigitUtils to generate this
-lon         <- '-99.3'
+lon         <- '-105.7'
 # The altitude above/below sea level, in meters
-altitude    <- '462'
+altitude    <- '1250'
 # The ISO3 country code for the country in which the population is located. 
 country     <- 'USA'
 # The continent that the population is located on. 
@@ -139,7 +139,7 @@ country     <- 'USA'
 continent   <- 'n_america'
 # The biome code
 #  https://patrickbarks.shinyapps.io/biomes/
-ecoregion   <- 'TGS'
+ecoregion   <- 'DES'
 
 
 # Main code --------------------------------------------------------------------

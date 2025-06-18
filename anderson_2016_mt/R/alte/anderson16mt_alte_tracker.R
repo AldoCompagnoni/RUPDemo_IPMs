ce807e99b3ad119f4b865969d4565cba9a4cc4d7
DianaSpurite <diana.spurite@posteo.de>
2025-06-18 11:20:49 +0200

    Updating tracker

--- anderson_2016_mt/R/alte/anderson16mt_alte_tracker.R
+++ anderson_2016_mt/R/alte/anderson16mt_alte_tracker.R
@@ -1,43 +1,36 @@
 # plantTracker - Anderson 2016 Montana - Allium textile
 
-
-# Author: Diana Spurite
+# Author: Diāna Spurīte
 # Co    : Aspen Workman, Aldo Compagnoni, Niklas Neisse
 # Email : diana.spurite@posteo.de
 # Main  : aldo.compagnoni@idiv.de
 # Web   : https://aldocompagnoni.weebly.com/
-# Date  : 2024.03.18
+# Date  : 2025.06.17
 
 # Publication: https://doi.org/10.1890/11-0193.1
 
 # rm(list = ls())
 
-
-# Data -------------------------------------------------------------------------
-# Define publication 
-author_year <- 'anderson_2016'
+# Specifications ---------------------------------------------------------------
+# Define publication
+v_author_year      <- c('anderson_2016')
 # Define region abbreviation
-region_abb <- 'mt'
-# Define growth form (grass - including c4, c3 and short grasses-, forb, shrub)
-gr_form    <- 'grass'
+v_region_abb       <- c('mt')
+# Define growth form (grass, forb, shrub, c4)
+v_gr_form          <- c('forb')
 # Customized delimiter for `read_delim` function, comma is predefined
-custom_delimiter <- c(',')
+v_custom_delimiter <- c(',')
 
+
+# Main pipelines ---------------------------------------------------------------
 source('pipeline/plant_tracker_01.R')
 
 # Select the x_th species (target species)
-head(sp_list, 20)
-target_spec <- sp_list %>% .[c(5),]  
-
-
-# Modifications to the data structure ------------------------------------------
-# Specific plots to exclude, a list of plots
-mod_plot <- c()
+head(sp_list, 10)
+target_spec <- sp_list %>% .[c(2),]  
 
 source('pipeline/plant_tracker_02.R')
 
-problem_quadrats <- unique(data$quadrat[!(data$year %in% inv$year)])
-print(problem_quadrats)
 
 # Exploration ------------------------------------------------------------------
 # Quadrat inventory
@@ -46,4 +39,7 @@ quad_inv
 dat_target_spec
 # Buffer size - regular and genet
 st_bbox(dat_target_spec)
+v_buff
+
+
 

214c78bebf1ab38cd0bb437192a405197685a211
NeisseN <neisse.n@protonmail.com>
2025-07-23 21:07:43 +0200

    generate pole data; plot growth; survival is not quite ready yet

--- archbold/R/pole/archbold_pole_data_exploration.R
+++ archbold/R/pole/archbold_pole_data_exploration.R
@@ -50,8 +50,8 @@ v_ggp_suffix    <- paste(
   tools::toTitleCase(v_head), '-', v_species)
 
 # Models
-v_mod_set_gr <- c()
 v_mod_set_su <- c()
+v_mod_set_gr <- c(2)
 v_mod_set_fl <- c()
 
 # Directory --------------------------------------------------------------------
@@ -75,6 +75,9 @@ if (!dir.exists(dir_result)) {dir.create(dir_result)}
 # Functions --------------------------------------------------------------------
 # function to plot your survival data 'binned' (instead of 'jittered')
 source('helper_functions/plot_binned_prop.R')
+source('helper_functions/line_color_pred_fun.R')
+source('helper_functions/predictor_fun.R')
+
 
 
 # Data -------------------------------------------------------------------------
@@ -139,16 +142,10 @@ df_org %>%
   theme(plot.subtitle = element_text(size = 8))
 
 
-# Working data -----------------------------------------------------------------
-df <- df_org %>% 
-  separate(col = date, into = c('year', 'month'), sep = '-')
-
-
 # Explore quad ------------------------------------------------------------------
 df_org %>% 
   group_by(unit) %>% 
   count(quad) %>% view()
-
 'Quad is indeed a unique identifier'
 
 
@@ -156,18 +153,218 @@ df_org %>%
 df_org %>% 
   separate(col = date, into = c('year', 'month'), sep = '-') %>% 
   mutate(id = paste0(unit, '_', quad, '_', id)) %>% 
-  arrange(unit, id, year, month) %>% view()
-
+  arrange(unit, id, year, month) #%>% view()
 ''
+
+# Exploring duplicates
+df_org %>%
+  separate(col = date, into = c('year', 'month'), sep = '-') %>%
+  mutate(id = paste0(unit, '_', quad, '_', id)) %>%
+  group_by(id, year, month) %>%
+  # Count number of entries per date per id
+  mutate(n_entries = n(),
+         unique_angle_dist = n_distinct(paste(angle, dist))) %>%
+  ungroup() %>%
+  # Keep only those individuals who have >1 record in at least one date
+  group_by(id) %>%
+  filter(any(n_entries > 1)) %>%
+  ungroup() %>%
+  arrange(unit, id, year, month) #%>% view()
+'The case of individual 1_609_109 indicates that 
+some plant IDs appear multiple times in the dataset, 
+occasionally with different angle and dist values. 
+In some instances, an individual is recorded as dead (survival == 0/ == 9), 
+and later, a new record with the same ID appears, 
+potentially with different spatial coordinates and its own associated growth measurements. 
+This pattern suggests that the same ID may be reused or reassigned to a new plant at a later time. 
+Therefore, I suggest to use angle/ distance as part of the ID.'
 
 # Data aggregation
 df_org %>% 
   # since there is no dormancy we can just filter for growth
   filter(!is.na(height)) %>% 
   separate(col = date, into = c('year', 'month'), sep = '-') %>% 
-  mutate(id = paste0(unit, '_', quad, '_', id)) %>% 
-  arrange(unit, id, year, month) %>% view()
-
+  mutate(id = paste0(unit, '_', quad, '_', id, '_', angle, '-', dist)) %>% 
+  arrange(unit, id, year, month) #%>% view()
 'We found that there are actually only about 6500 observations in total'
 
+# Exploring recruits
+df_org %>% 
+  filter(survival < 9,
+         !c(survival == 1 & is.na(height))) %>% 
+  separate(col = date, into = c('year', 'month'), sep = '-') %>% 
+  mutate(id = paste0(unit, '_', quad, '_', id, '_', angle, '-', dist)) %>% 
+  arrange(unit, id, year, month) #%>% view()
+'There are recruits (assuming survival == 5 indicates a new recruit) 
+that die without ever having a recorded height. 
+These individuals should be excluded, as they can distort our analysis of recruit survival. 
+Specifically, they contribute to the total count of recruits at the plot level but 
+do not provide any data for survival within the recruitment size category.'
 
+# Exploring plant death
+df_org %>% 
+  filter(survival == 0 | !is.na(height)) %>% 
+  separate(col = date, into = c('year', 'month'), sep = '-') %>% 
+  mutate(id = paste0(unit, '_', quad, '_', id, '_', angle, '-', dist)) %>% 
+  arrange(unit, id, year, month) #%>% view()
+''
+
+
+# Generating data --------------------------------------------------------------
+df_gen <- df_org %>% 
+  separate(col = date, into = c('year', 'month'), sep = '-') %>% 
+  mutate(id = paste0(unit, '_', quad, '_', id, '_', angle, '-', dist)) %>% 
+  arrange(unit, id, year, month) %>%
+  # Fire: For now we will just drag the maximum value of each year with us
+  select(!c(angle, dist)) %>% #view()
+  group_by(id, year) %>%
+  mutate(postburn_plant = if_else(all(is.na(postburn_plant)), NA_real_,
+                                  max(postburn_plant, na.rm = TRUE))) %>%
+  ungroup() %>% #view()
+  filter(survival < 9) %>% 
+  # Recruits: 5 indicates that the plant was detected for the first time. 
+  #  however, there are plants that were detected in a month without size measurement
+  #  many of them are present in the next year too.. for now I will count them as recruits too
+  #  actually I can give them a 2 - I like it, easy to change later too..
+  # Furthermore, 1_609_101_100-19 is an example of how it is not considered a recruit in the original data set
+  #  if the plant was there already in the fist year of the sampling campaign (i.e. 2001)
+  #  We also don't know if it was or wasn't a recruit and thus it gets an NA
+  group_by(id) %>%
+  mutate(
+    recruit = if_else(
+      (lag(survival) == 5 & is.na(lag(stage))), 2, 0, missing = 0)) %>% 
+  mutate(recruit = if_else(survival == 5, 1, recruit)) %>% 
+  mutate(recruit = if_else(as.numeric(year) == 2001, NA_real_, recruit)) %>% #view()
+  filter(survival < 5) %>% 
+  ungroup() %>%
+  # Survival: 
+  #  Since there is no dormancy I can remove everything that does not have a size 
+  # !!!!!!! there is survival 3, what is that? !!!!!!!!!!!!!!!!!
+  filter(!is.na(stage)) %>% #view()
+  group_by(id) %>%
+    mutate(
+      survival = if_else(row_number() == n(), 0, survival)) %>%
+    ungroup() %>% #view()
+  # Growth:
+  group_by(id) %>%
+  mutate(size_t1 = lead(height)) %>%
+  ungroup() #%>% view()
+
+# Working data -----------------------------------------------------------------
+df <- df_gen %>% 
+  rename(survives = survival,
+         size_t0  = height,
+         site     = unit) %>% 
+  mutate(logsize_t0   = log(size_t0),
+         logsize_t1   = log(size_t1),    
+         logsize_t0_2 = logsize_t0^2,     
+         logsize_t0_3 = logsize_t0^3) %>% 
+  select(site, quad, cohort, id, year, 
+         stage, survives, size_t0, flowering_stems, recruit, 
+         size_t1, logsize_t1, logsize_t0, logsize_t0_2, logsize_t0_3,
+         crown_diameter, stems)
+
+
+# Survival ---------------------------------------------------------------------
+df_su <- df %>% 
+  filter(!is.na(survives)) %>%
+  filter(size_t0 != 0) %>%
+  dplyr::select(id, year, size_t0, survives, size_t1, 
+                logsize_t0, logsize_t1, logsize_t0_2, logsize_t0_3)
+
+fig_su_overall <- ggplot(
+  data = plot_binned_prop(df, 10, logsize_t0, survives)) +
+  geom_jitter(data = df_su, aes(x = logsize_t0, y = survives), 
+              position = position_jitter(width = 0.1, height = 0.3)
+              , alpha = .1) +
+  geom_point(aes(x = logsize_t0, y = survives),
+             alpha = 1, pch = 16, color = 'red') +
+  geom_errorbar(aes(x = logsize_t0, ymin = lwr, ymax = upr),
+                linewidth = 0.5, width = 0.5) +
+  scale_y_continuous(breaks = c(0.1, 0.5, 0.9), limits = c(0, 1.01)) +
+  theme_bw() +
+  theme(axis.text = element_text(size = 8),
+        title = element_text(size = 10),
+        plot.subtitle = element_text(size = 8)) +
+  labs(title = 'Survival',
+       subtitle = v_ggp_suffix,
+       x = expression('log(size)'[t0]),
+       y = expression('Survival to time t1'))
+fig_su_overall 
+
+
+# Growth data ------------------------------------------------------------------
+df_gr <- df %>% 
+  subset(size_t0 != 0) %>%
+  subset(size_t1 != 0) %>% 
+  dplyr::select(id, year, size_t0, size_t1,
+                logsize_t0, logsize_t1, logsize_t0_2, logsize_t0_3)
+
+ggplot(
+  data  = df_gr, aes(x = logsize_t0, y = logsize_t1)) +
+  geom_point(alpha = 0.5, pch = 16, size = 0.7, color = 'red') +
+  theme_bw() +
+  theme(axis.text = element_text(size = 8),
+        title     = element_text(size = 10)) +
+  labs(title    = 'Growth',
+       subtitle = v_ggp_suffix,
+       x        = expression('log(size) ' [t0]),
+       y        = expression('log(size)  '[t1])) +
+  theme(plot.subtitle = element_text(size = 8))
+
+
+# Growth model -----------------------------------------------------------------
+# Intercept model 
+mod_gr_0 <- lm(logsize_t1 ~ 1,
+               data = df_gr)
+# Linear model
+mod_gr_1 <- lm(logsize_t1 ~ logsize_t0, 
+               data = df_gr)
+# Quadratic model
+mod_gr_2 <- lm(logsize_t1 ~ logsize_t0 + logsize_t0_2, 
+               data = df_gr)  
+# Cubic model
+mod_gr_3 <- lm(logsize_t1 ~ logsize_t0 + logsize_t0_2 + logsize_t0_3, 
+               data = df_gr)
+
+mods_gr      <- list(mod_gr_0, mod_gr_1, mod_gr_2, mod_gr_3)
+mods_gr_dAIC <- AICtab(mods_gr, weights = T, sort = F)$dAIC
+
+# Get the sorted indices of dAIC values
+mods_gr_sorted <- order(mods_gr_dAIC)
+
+# Establish the index of model complexity
+if (length(v_mod_set_gr) == 0) {
+  mod_gr_index_bestfit <- mods_gr_sorted[1]
+  v_mod_gr_index       <- mod_gr_index_bestfit - 1 
+} else {
+  mod_gr_index_bestfit <- v_mod_set_gr +1
+  v_mod_gr_index       <- v_mod_set_gr
+}
+
+mod_gr_bestfit         <- mods_gr[[mod_gr_index_bestfit]]
+mod_gr_ranef           <- coef(mod_gr_bestfit)
+
+# Predict size at time t1 using the mean growth model
+df_gr$pred <- predict(mod_gr_bestfit, type = 'response')
+
+# Growth plot
+fig_gr_line <- ggplot(
+  df_gr, aes(x = logsize_t0, y = logsize_t1)) +
+  # Plot observed data
+  geom_point() +
+  geom_function(fun = function(x) predictor_fun(x, mod_gr_ranef), 
+                color = line_color_pred_fun(mod_gr_ranef), lwd = 2) +
+  theme_bw() + 
+  labs(title    = 'Growth prediction',
+       subtitle = v_ggp_suffix) +
+  theme(plot.subtitle = element_text(size = 8))
+
+fig_gr_pred <- ggplot(
+  df_gr, aes(x = pred, y = logsize_t1)) +
+  geom_point() +  
+  geom_abline(aes(intercept = 0, slope = 1), color = 'red', lwd = 2) + 
+  theme_bw()
+
+fig_gr <- fig_gr_line + fig_gr_pred + plot_layout() 
+fig_gr
